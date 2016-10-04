57d0fe9529a0f6a8765b1b36cbf3d1256b2eb5bd
fervil88 <fernandovillar88@gmail.com>
2016-10-03 21:07:37 -0300

    methods added

--- app/src/main/java/com/harriague/curso/myapplication/MainActivity.java
+++ app/src/main/java/com/harriague/curso/myapplication/MainActivity.java
@@ -166,4 +166,34 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
 }
