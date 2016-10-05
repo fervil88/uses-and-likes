016a5f816a439ed5b2c56a0efbd6f4761226fad3
Emiliano Castells <emi.castells@gmail.com>
2016-10-05 20:17:10 -0300

    Update GSP Service

--- app/src/main/java/com/harriague/curso/services/GPSTrackerService.java
+++ app/src/main/java/com/harriague/curso/services/GPSTrackerService.java
@@ -1,15 +1,25 @@
 package com.harriague.curso.services;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Emi on 04/10/2016.
@@ -46,24 +56,46 @@ public class GPSTrackerService extends Service implements LocationListener {
     public Location getLocation() {
         try {
             locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
-
-            // getting GPS status
             isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-            // getting network status
             isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // no network provider is enabled
-            } else {
+
+            // Get location from Network Provider
+            if (isNetworkEnabled) {
                 this.canGetLocation = true;
-                // First get location from Network Provider
-                if (isNetworkEnabled) {
-                   // Think about that
+                // Think about enabled permission
+             /*   locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_GPS_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_GPS_UPDATES, this); */
+                if (locationManager != null) {
+                    // Think about enabled permission
+                    /*  location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER); */
+                    if (location != null) {
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                    }
                 }
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    // Think about that
+
+            }
+            // if GPS Enabled get lat/long using GPS Services
+            if (isGPSEnabled) {
+                this.canGetLocation = true;
+                if (location == null) {
+                    // Think about enabled permission
+                  /*  locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_GPS_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_GPS_UPDATES, this); */
+                    Log.d("GPS Enabled", "GPS Enabled");
+                    if (locationManager != null) {
+                        // Think about enabled permission
+                        /* location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER); */
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                        }
+                    }
                 }
             }
 
@@ -74,6 +106,37 @@ public class GPSTrackerService extends Service implements LocationListener {
         return location;
     }
 
+
+    public void stopUsingGPS(){
+        if(locationManager != null){
+            // Think about enabled permission
+            /*locationManager.removeUpdates(GPSTrackerService.this);*/
+        }
+    }
+
+    public String getAddress() {
+        String result = "";
+        if (location != null && location.getLatitude() != 0.0 && location.getLongitude() != 0.0) {
+            Log.d(TAG, "loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0");
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Log.d(TAG, "!list.isEmpty()");
+                    Address address = list.get(0);
+                    result = address.getAddressLine(0);
+
+                }
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+        return result;
+    }
+
+    public void setLocation(Location location) {
+
+    }
 
     @Nullable
     @Override
